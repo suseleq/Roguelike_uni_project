c39a7bc865c1c86dfec5377cda51e8f7237ea44d
suseleq <kubenowak@gmail.com>
2023-05-14 17:51:21 +0200

    game Class

--- Game.cpp
+++ Game.cpp
@@ -1,1 +1,47 @@
 #include "Game.h"
+
+void Game::initWindow()
+{
+	this->window = std::make_unique<sf::RenderWindow>(sf::VideoMode(1024, 900), "siema");
+}
+
+void Game::updateSfmlEvent()
+{
+	while (this->window.get()->pollEvent(e))
+	{
+		if(this->e.type == sf::Event::Closed)
+			this->window.get()->close();
+	}
+}
+
+Game::Game()
+{
+	this->dt = 0.f;
+	this->initWindow();
+}
+
+Game::~Game()
+{
+}
+
+void Game::uptade()
+{
+	this->dt = this->dtClock.restart().asSeconds();
+	this->updateSfmlEvent();
+}
+
+void Game::render()
+{
+	this->window.get()->clear(sf::Color(213, 41, 76));
+
+	this->window.get()->display();
+}
+
+void Game::run()
+{
+	while (this->window.get()->isOpen())
+	{
+		this->uptade();
+		this->render();
+	}
+}
--- Game.h
+++ Game.h
@@ -1,5 +1,29 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <memory>
 class Game
 {
+private:
+	std::unique_ptr<sf::RenderWindow> window;
+
+
+	sf::Event e;
+	sf::Clock dtClock;
+	float dt;
+
+
+	void initWindow();
+	void updateSfmlEvent();
+
+
+public:
+	Game();
+	~Game();
+
+
+	void uptade();
+	void render();
+	void run();
 };
 
